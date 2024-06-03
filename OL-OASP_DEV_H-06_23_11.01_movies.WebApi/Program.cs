84966f6525af2604cb6aeb167fa6c49572dfa452
Ivan Radoš <irados@autozubak.hr>
2024-06-03 19:03:15 +0200

    Add project files.

---
+++ OL-OASP_DEV_H-06_23_11.01_movies.WebApi/Program.cs
@@ -0,0 +1,37 @@
+
+namespace OL_OASP_DEV_H_06_23_11._01_movies.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ OL-OASP_DEV_H-06_23_11.01_movies.WebApi/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace OL_OASP_DEV_H_06_23_11._01_movies.WebApi
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
